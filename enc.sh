396a2dd43414b7c18a0de49c4d5b3b4cff2ea673
saurabhterna <saurabh.terna@gmail.com>
2022-09-15 07:45:13 +0530

    Update enc.sh

--- enc.sh
+++ enc.sh
@@ -1,10 +1,10 @@
-echo "Please enter the KMS Endpoint URL"
+cho "Please enter the KMS Endpoint URL"
 read ENDPOINT
 echo "Please enter the KMS Key OCID"
 read KEY
 echo "Please enter the text you wish to encrypt"
 read PLAIN_TEXT
 echo "Encrypted Text"
-encrypted=$(oci kms crypto encrypt --key-id "$KEY" --endpoint "$ENDPOINT" --pla$
+encrypted=$(oci kms crypto encrypt --key-id "$KEY" --endpoint "$ENDPOINT" --plaintext "$( echo $PLAIN_TEXT | base64 -w0 )")
 export cipher=$(echo $encrypted | jq -r '.data | .ciphertext')
 echo $cipher
