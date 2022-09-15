0eaa48b72703592db25451a0557b5165a7d0a643
saurabhterna <saurabh.terna@gmail.com>
2022-09-15 16:14:22 +0530

    Update ocivault-decrypt.sh

--- ocivault-decrypt.sh
+++ ocivault-decrypt.sh
@@ -1,11 +1,13 @@
-echo "Please enter the KMS Endpoint URL"
+echo "Please enter the OCI Vault Cryptographic Endpoint URL"
 read ENDPOINT
-echo "Please enter the KMS Key OCID"
+echo "Please enter your Master Encryption Key OCID"
 read KEY
-echo "Please enter the Encrypted Text (Above)"
+echo "Please enter the Encrypted Text (Genreated Above)"
 read CIPHER_TEXT
-echo "Plain Text"
+echo "---------- Output ----------"
 decrypted=$(oci kms crypto decrypt --key-id "$KEY" --endpoint "$ENDPOINT" --ciphertext $CIPHER_TEXT)
 echo $decrypted
 export plain=$(echo $decrypted | jq -r '.data | .plaintext')
+echo "---------- Plain Text ----------"
 echo $plain | base64 --decode
+echo "------------------------------------"
